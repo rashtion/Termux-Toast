4b47a67757162c41d7b76fb9ae1b6c10341b083c
rashtion <64362298+rashtion@users.noreply.github.com>
2020-06-30 21:15:26 +0300

    Update read.py

--- read.py
+++ read.py
@@ -1,3 +1,4 @@
 # lol
 
 print("Thanks for using, if you wanna use matrix? command : bash matrix.sh")
+print("If you want a information for Matrix ? please install the TERMUX:API from Google PLay Store. And use the : bash infomatrix.sh ")
