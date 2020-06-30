6460e71b6d8847260e4ac57122940077a5582fcf
rashtion <64362298+rashtion@users.noreply.github.com>
2020-06-30 21:23:43 +0300

    Create infomatrix.sh

---
+++ infomatrix.sh
@@ -0,0 +1,20 @@
+pkg install cmatrix
+pkg install colorama
+pkg install toilet
+pkg install termux-api
+pkg install requests
+clear
+termux-setup-storage > /dev/null 2>&1 &
+sleep 3
+termux-toast -b red -c yellow "Hi!"
+	sleep 2
+	termux-toast -b black -c white  "I'm just a artificial intelligence! my developer : rashtion"
+	sleep 2
+	termux-toast -b black -c white   "Did you know that the codes flowing on the screen are just sushi recipes?"
+	sleep 4
+	termux-toast -b black -c white  "It's so epic, lol xD"
+	sleep 3
+	termux-toast -b black -c white  "Allright, have fun!)"
+	sleep 2
+	termux-toast -b black -c white  "github.com/rashtion"
+	sleep 2
